e20b4d85e75bcb2a0ed548bf5ee2bd1f3c9739cb
fgolfc <cf.ts.fujimoto@gmail.com>
2023-08-31 10:29:14 +0900

    24th commit

--- app/javascript/packs/response_notifications.js
+++ app/javascript/packs/response_notifications.js
@@ -1,69 +1,66 @@
 document.addEventListener('turbolinks:load', function() {
-  const responseNotifyAdminButtons = document.querySelectorAll('[data-response-id]');
+    const responseNotifyAdminButtons = document.querySelectorAll('[data-response-id]');
 
-  function hideElement(el) {
-      el.style.opacity = '0';
-      el.style.visibility = 'hidden';
-  }
+    function hideElement(el) {
+        el.style.opacity = '0';
+        el.style.visibility = 'hidden';
+    }
 
-  function showElement(el) {
-      el.style.opacity = '1';
-      el.style.visibility = 'visible';
-  }
+    function showElement(el) {
+        el.style.opacity = '1';
+        el.style.visibility = 'visible';
+    }
 
-  responseNotifyAdminButtons.forEach((notifyAdminButton) => {
-      const responseId = notifyAdminButton.getAttribute('data-response-id');
+    responseNotifyAdminButtons.forEach((notifyAdminButton) => {
+        const responseId = notifyAdminButton.getAttribute('data-response-id');
 
-      if (!notifyAdminButton.dataset.listenerAdded) {
-          notifyAdminButton.dataset.listenerAdded = 'true'; 
+        if (!notifyAdminButton.dataset.listenerAdded) {
+            notifyAdminButton.dataset.listenerAdded = 'true'; 
 
-          notifyAdminButton.addEventListener('click', function(event) {
-              hideElement(notifyAdminButton);
-              
-              const responseNotificationForm = document.getElementById(`notification-form-${responseId}`);
-              const commentSection = document.getElementById(`comment-section-${responseId}`);
-              const submitButton = document.getElementById(`submit-btn-${responseId}`);
-              const hiddenResponseIdField = document.getElementById(`response-id-field-${responseId}`);
-              const centerContainer = document.querySelector('.center-container');
+            notifyAdminButton.addEventListener('click', function(event) {
+                hideElement(notifyAdminButton);
+                
+                const responseNotificationForm = document.getElementById(`notification-form-${responseId}`);
+                const commentSection = document.getElementById(`comment-section-${responseId}`);
+                const submitButton = document.getElementById(`submit-btn-${responseId}`);
+                const hiddenResponseIdField = document.getElementById(`response-id-field-${responseId}`);
+                const centerContainer = document.querySelector('.center-container');
 
-              if (hiddenResponseIdField) {
-                  hiddenResponseIdField.value = responseId;
-              } else {
-                  console.error(`Element with ID response-id-field-${responseId} not found.`);
-                  return;
-              }
-              
-              showElement(responseNotificationForm);
-              showElement(commentSection);
-              showElement(submitButton);
+                if (hiddenResponseIdField) {
+                    hiddenResponseIdField.value = responseId;
+                } else {
+                    console.error(`Element with ID response-id-field-${responseId} not found.`);
+                    return;
+                }
 
-              if (centerContainer) {
-                  centerContainer.style.height = 'auto';
-              }
+                showElement(responseNotificationForm);
+                showElement(commentSection);
+                showElement(submitButton);
 
-              if (!responseNotificationForm.dataset.listenerAdded) {
-                  responseNotificationForm.dataset.listenerAdded = 'true';
+                if (centerContainer) {
+                    centerContainer.style.height = 'auto';
+                }
 
-                  responseNotificationForm.addEventListener('ajax:success', function() {
-                      alert("通知が成功しました");
-                      hideElement(responseNotificationForm);
-                      hideElement(commentSection);
-                      hideElement(submitButton);
-                      showElement(notifyAdminButton);
+                // 条件文を削除し、直接イベントリスナを追加
+                responseNotificationForm.addEventListener('ajax:success', function() {
+                    alert("通知が成功しました");
+                    hideElement(responseNotificationForm);
+                    hideElement(commentSection);
+                    hideElement(submitButton);
+                    showElement(notifyAdminButton);
 
-                      if (centerContainer) {
-                          centerContainer.style.height = '0';
-                      }
-                  });
+                    if (centerContainer) {
+                        centerContainer.style.height = '0';
+                    }
+                });
 
-                  responseNotificationForm.addEventListener('ajax:error', function(event) {
-                      const detail = event.detail;
-                      const errorMessages = detail[0]['errors'] || [];
-                      alert(errorMessages.join(", "));
-                      showElement(notifyAdminButton);
-                  });
-              }
-          });
-      }
-  });
+                responseNotificationForm.addEventListener('ajax:error', function(event) {
+                    const detail = event.detail;
+                    const errorMessages = detail[0]['errors'] || [];
+                    alert(errorMessages.join(", "));
+                    showElement(notifyAdminButton);
+                });
+            });
+        }
+    });
 });
--- spec/system/post_likes_spec.rb
+++ spec/system/post_likes_spec.rb
@@ -9,11 +9,13 @@ RSpec.describe 'PostLikes', type: :system do
     driven_by(:selenium_chrome)
     sign_in user
     visit posts_path # ここを変更して、投稿一覧ページに移動します
+
+    expect(page).to have_content(post.body)
   end
 
   describe 'liking a post' do
     it 'allows a user to like a post and updates the UI accordingly' do
-      within "#like-button-#{post.id}" do
+      within "#like-button-post-#{post.id}" do
         click_on '❤️ いいね'
       end
       
@@ -21,7 +23,7 @@ RSpec.describe 'PostLikes', type: :system do
         # '❤️ いいね' ボタンがクリックされた後の変更を確認
       }.to change(PostLike, :count).by(1)
 
-      within "#like-button-#{post.id}" do
+      within "#like-button-post-#{post.id}" do
         expect(page).to have_content('いいねをキャンセル') # 期待するボタンの変更
       end
       expect(page).to have_selector("#like-count-#{post.id}", text: '1') # 期待するいいねのカウント数
@@ -35,7 +37,7 @@ RSpec.describe 'PostLikes', type: :system do
     end
 
     it 'allows a user to unlike a post and updates the UI accordingly' do
-      within "#like-button-#{post.id}" do
+      within "#like-button-post-#{post.id}" do
         click_on 'いいねをキャンセル'
       end
 
@@ -43,7 +45,7 @@ RSpec.describe 'PostLikes', type: :system do
         # 'いいねをキャンセル' ボタンがクリックされた後の変更を確認
       }.to change(PostLike, :count).by(-1)
 
-      within "#like-button-#{post.id}" do
+      within "#like-button-post-#{post.id}" do
         expect(page).to have_content('❤️ いいね') # '❤️ いいね' ボタンに戻ることを確認
       end
       expect(page).to have_selector("#like-count-#{post.id}", text: '0') # 期待するいいねのカウント数
