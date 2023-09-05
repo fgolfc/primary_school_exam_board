5c193d292fd097c272da82f3ff737a940b4c4374
fgolfc <cf.ts.fujimoto@gmail.com>
2023-09-05 21:21:40 +0900

    28th commit

--- db/migrate/20230904025333_remove_post_id_from_response_likes.rb
+++
@@ -1,7 +0,0 @@
-class RemovePostIdFromResponseLikes < ActiveRecord::Migration[6.0]
-  def change
-    if column_exists?(:response_likes, :post_id)
-      remove_column :response_likes, :post_id, :integer
-    end
-  end  
-end
