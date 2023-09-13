3aebe62d722048341da2713f4dd6df038e9c60ad
fgolfc <cf.ts.fujimoto@gmail.com>
2023-09-13 19:29:02 +0900

    42th commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: { host: 'your_app_domain.com'}}
+  config.action_mailer.default_url_options = { host: { host: '54.249.195.174'}}
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
