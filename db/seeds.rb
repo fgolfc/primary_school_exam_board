b3457a44416921079535c355f6b411338315536f
fgolfc <cf.ts.fujimoto@gmail.com>
2023-09-23 09:45:08 +0900

    56th commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 管理者ユーザーを作成
-admin = User.create!(
-  email: 'adminadmin@example.com',
-  password: 'password',
-  password_confirmation: 'password',
-  admin: true,
-  nickname: 'Admin',
-  prefecture: :tokyo_chuoku
-  confirmed_at: Time.now
-)
-
-# 一般ユーザーを作成
-user1 = User.create!(
-  email: 'motheruser1@example.com',
-  password: 'password',
-  password_confirmation: 'password',
-  nickname: 'User1',
-  prefecture: :tokyo_shinjukuku
-  confirmed_at: Time.now
-)
-
-user2 = User.create!(
-  email: 'motheruser2@example.com',
-  password: 'password',
-  password_confirmation: 'password',
-  nickname: 'User2',
-  prefecture: :kanagawa
-  confirmed_at: Time.now
-)
