bbb957e507e9110226f49163cbd3919e2dbdcfcb
fgolfc <cf.ts.fujimoto@gmail.com>
2023-09-25 17:03:07 +0900

    62th commit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,19 @@
 class Users::SessionsController < Devise::SessionsController
-  # ここにカスタムロジックやアクションを追加
+
+  # def new
+  #   super
+  # end
+
+  # def create
+  #   super
+
+  #   if params[:user][:type] == "guest"
+  #     sign_in(:user, Guest.new)
+  #     redirect_to root_path, notice: "ゲストとしてログインしました。"
+  #   elsif params[:user][:type] == "admin"
+  #     sign_in(:user, Admin.new)
+  #     redirect_to admin_root_path, notice: "管理者としてログインしました。"
+  #   end
+  # end
+
 end
